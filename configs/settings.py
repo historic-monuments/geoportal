b911303edc7f441d11bbb2f6c9e194c379a27851
historic-monuments <taras.sakharchuk@gmail.com>
2023-12-20 06:16:31 +0200

    Disable debug

--- configs/settings.py
+++ configs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2uk5#dgb1&u^$h)i62zdc=nnt_9_kjjsme4*540a4lle*=o4x8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
