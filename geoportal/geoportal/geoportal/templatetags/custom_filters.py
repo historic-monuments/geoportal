c2d16ece9eb73c5dbcf63350a25ac0dd8e610ab2
unknown <taras.sakharchuk@gmail.com>
2023-12-20 03:18:08 +0200

    Change project structure

--- geoportal/geoportal/geoportal/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='first_sentence')
-def first_sentence(text):
-    sentences = text.split('.')
-    return sentences[0] if sentences else ''
